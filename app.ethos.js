2f02ecdc8f8c4daa87024c7081dc8e9f1d660e4c
Parus1832 <voilier.de.rebellion@gmail.com>
2026-05-02 04:37:36 +0900

    データエクスポート/インポート

--- app.ethos.js
+++ app.ethos.js
@@ -166,3 +166,38 @@ document.getElementById("next-month").addEventListener("click", () => {
   }
   buildCalendar();
 });
+
+function exportData() {
+  const data = {};
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    data[key] = localStorage.getItem(key);
+  }
+  const blob = new Blob([JSON.stringify(data, null, 2)], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "ethos-backup.json";
+  a.click();
+  URL.revokeObjectURL(url);
+}
+
+function importData() {
+  const input = document.createElement("input");
+  input.type = "file";
+  input.accept = ".json";
+  input.onchange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const data = JSON.parse(e.target.result);
+      Object.keys(data).forEach(key => {
+        localStorage.setItem(key, data[key]);
+      });
+      alert("復元完了！");
+      buildCalendar();
+    };
+    reader.readAsText(file);
+  };
+  input.click();
+}
